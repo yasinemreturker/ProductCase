72f6b0e1c5b5a55daea6a51e0eabb70782c2a0db
byTurker <mr_turker@hotmail.com>
2022-02-02 02:43:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/productcase/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.productcase.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.productcase.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
